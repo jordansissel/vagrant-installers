473c39d16fcddec1a64378cb1a5d7dea5e706745
Jordan Sissel <jls@semicomplete.com>
2012-06-11 23:30:52 -0700

    Set checksums path, otherwise chef tries to mkdir /etc/chef

--- config/solo.rb
+++ config/solo.rb
@@ -2,6 +2,7 @@ require 'pathname'
 require 'tmpdir'
 root_dir = Pathname.new(File.expand_path("../../", __FILE__))
 
+checksum_path root_dir.join("checksums").to_s
 cookbook_path root_dir.join("cookbooks").to_s
 file_cache_path root_dir.join("cache").to_s
 log_level :info
